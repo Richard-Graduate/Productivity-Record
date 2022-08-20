01f52241306aee5dba567e8daafc86fff4890a36
Richard-Graduate <i59709063202@gmail.com>
2022-08-20 16:43:13 +0800

    0820_1645_OK

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "C++ TEST",
+        "excerpt":" ","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/template/",
+        "teaser": null
+      }]
