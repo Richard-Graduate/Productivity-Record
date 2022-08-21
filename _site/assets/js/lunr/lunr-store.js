8c574f93b9b20301a940a596227eda390819951d
Richard-Graduate <i59709063202@gmail.com>
2022-08-21 18:26:09 +0800

    sample c

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "C++ HEHE",
+        "excerpt":" ","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/template/",
+        "teaser": null
+      }]
